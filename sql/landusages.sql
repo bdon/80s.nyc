fdafeff6face1213c5ff62ef28737ce55b4cb600
Brandon Liu <bdon@bdon.org>
2017-09-11 13:17:48 -0400

    add OSM SQL queries

---
+++ sql/landusages.sql
@@ -0,0 +1,5 @@
+select json_build_object(
+  'type','Feature',
+  'geometry',st_asgeojson(geometry)::json,
+  'properties',to_jsonb(row) - 'id' - 'geometry' 
+) FROM (SELECT * FROM import.osm_landusages) row;
