6cb5b0244af756d383ba5966a4d28a9395e2140e
Brandon Liu <bdon@bdon.org>
2020-02-18 12:00:13 -0500

    fixup about page

--- www/src/Intro.js
+++ www/src/Intro.js
@@ -4,17 +4,19 @@ import './Intro.css'
 class Intro extends React.Component {
   render() {
     return (
-      <div id="introContainer" onClick={this.props.dismissIntro}>
-        <div id="intro">
-          <h1>80s.NYC</h1>
-          <h2>STREET VIEW OF 1980s NEW YORK</h2>
-          <img className="introStrip" src="/images/strip.jpg" alt=""></img>
-          <p>Explore over 100,000 street segments
-          and 800,000 building photos.</p>
-          <p>Click near a highlighted segment to see the street,
-      or select a featured <strong>story</strong> from the menu above.</p>
-          <img className="introGif" src="/images/intro.gif" alt=""></img> 
-          <a>START EXPLORING</a>
+      <div id="introContainer" className="vh-100 flex" onClick={this.props.dismissIntro}>
+        <div className="flex items-center justify-center w-100 lh-copy"> 
+          <div id="intro" className="ph3 pv4 mw6">
+            <div className="f2">80s.NYC</div>
+            <div className="f3 mt3">STREET VIEW OF 1980s NEW YORK</div>
+            <img className="introStrip mt3" src="/images/strip.jpg" alt=""></img>
+            <p>Explore over 100,000 street segments
+            and 800,000 building photos.</p>
+            <p>Click near a highlighted segment to see the street,
+        or select a featured <strong>story</strong> from the menu above.</p>
+            <div><img className="introGif" src="/images/intro.gif" alt=""></img> </div>
+            <div className="f3 mt3">START EXPLORING</div>
+          </div>
         </div>
       </div>
     );
--- www/src/index.js
+++ www/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import 'tachyons/css/tachyons.css';
 
 ReactDOM.render(
   <App />,
