265792b8a8bcf6827db039ce3e057c2c5818fbc5
Brandon Liu <bdon@bdon.org>
2020-02-18 16:37:05 -0500

    css cleanup

--- www/src/Intro.js
+++ www/src/Intro.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import './Intro.css'
 
 class Intro extends React.Component {
   render() {
     return (
-      <div id="introContainer" className="vh-100 flex" onClick={this.props.dismissIntro}>
+      <div className="vh-100 flex z-9999 fixed top-0 bottom-0 left-0 right-0" style={{backgroundColor:"rgba(0,0,0,0.3)"}} onClick={this.props.dismissIntro}>
         <div className="flex items-center justify-center w-100 lh-copy"> 
-          <div id="intro" className="ph3 pv4 mw6">
+          <div id="intro" className="pa3 mw6 tc" style={{backgroundColor:"#2c1f27"}}>
             <div className="f2">80s.NYC</div>
-            <div className="f3 mt3">STREET VIEW OF 1980s NEW YORK</div>
+            <div className="f4 mt3">STREET VIEW OF 1980s NEW YORK</div>
             <img className="introStrip mt3" src="/images/strip.jpg" alt=""></img>
             <p>Explore over 100,000 street segments
             and 800,000 building photos.</p>
             <p>Click near a highlighted segment to see the street,
         or select a featured <strong>story</strong> from the menu above.</p>
-            <div><img className="introGif" src="/images/intro.gif" alt=""></img> </div>
+            <div><img className="w4" src="/images/intro.gif" alt=""></img> </div>
             <div className="f3 mt3">START EXPLORING</div>
           </div>
         </div>
