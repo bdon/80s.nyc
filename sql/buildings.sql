e44bd9a369545f5a39d8fa7afd1a724fbdb24d02
Brandon Liu <bdon@bdon.org>
2020-02-19 17:42:39 -0500

    remove OSM basemap data

--- sql/buildings.sql
+++
@@ -1,5 +0,0 @@
-select json_build_object(
-  'type','Feature',
-  'geometry',st_asgeojson(geometry)::json,
-  'properties',to_jsonb(row) - 'id' - 'geometry' 
-) FROM (SELECT * FROM import.osm_buildings) row;
